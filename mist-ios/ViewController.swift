90020d2ba00f6ebdefbf541dbbe5c753d4ab677b
adamvnovak <adamvnovak@protonmail.com>
2022-02-25 23:36:36 -0800

    Initial Commit

---
+++ mist-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mist-ios
+//
+//  Created by Adam Novak on 2022/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
