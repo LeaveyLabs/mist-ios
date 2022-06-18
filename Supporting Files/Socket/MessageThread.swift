02850b8f5a92b8e3388780c48ff2f6010b11fc2c
kevinsun127 <kevinsun@usc.edu>
2022-06-17 19:41:50 -0700

    sender and receiver not to_user and from_user

--- Supporting Files/Socket/MessageThread.swift
+++ Supporting Files/Socket/MessageThread.swift
@@ -10,28 +10,28 @@ import Starscream
 
 class MessageThread: WebSocketDelegate {
     
-    var from_user: String;
-    var to_user: String;
+    var sender: String;
+    var receiver: String;
     var unsent_messages: [String];
     var server_messages: [Message];
     var init_data: Data;
     var socket: WebSocket;
     var connected: Bool;
     
-    init(from_user: String, to_user: String) throws {
-        self.from_user = from_user
-        self.to_user = to_user
+    init(sender: String, receiver: String) throws {
+        self.sender = sender
+        self.receiver = receiver
         self.unsent_messages = []
         self.server_messages = []
         self.connected = false
         
         let params:[String:String] = ["type": "init",
-                                      "from_user": self.from_user,
-                                      "to_user": self.to_user]
+                                      "sender": self.sender,
+                                      "receiver": self.receiver]
         let json = try JSONEncoder().encode(params)
         self.init_data = json
         
-        let request = URLRequest(url: URL(string: "ws://localhost:8001/")!)
+        let request = URLRequest(url: URL(string: "wss://mist-chat-test.herokuapp.com/")!)
         self.socket = WebSocket(request: request)
         self.socket.delegate = self
         self.socket.connect()
@@ -45,9 +45,10 @@ class MessageThread: WebSocketDelegate {
         // If we're connected, then send it
         if (connected) {
             let params:[String:String] = ["type": "message",
-                                          "from_user":self.from_user,
-                                          "to_user":self.to_user,
-                                          "text": message_text]
+                                          "sender":self.sender,
+                                          "receiver":self.receiver,
+                                          "body": message_text,
+                                          "token": getGlobalAuthToken()]
             let json = try JSONEncoder().encode(params)
             self.socket.write(data:json)
         }
@@ -60,9 +61,10 @@ class MessageThread: WebSocketDelegate {
     func clearUnsentMessages() {
         for unsent_message in unsent_messages {
             let params:[String:String] = ["type": "message",
-                                          "from_user":self.from_user,
-                                          "to_user":self.to_user,
-                                          "text": unsent_message]
+                                          "from_user":self.sender,
+                                          "to_user":self.receiver,
+                                          "text": unsent_message,
+                                          "token": getGlobalAuthToken()]
             do {
                 let json = try JSONEncoder().encode(params)
                 self.socket.write(data:json)
