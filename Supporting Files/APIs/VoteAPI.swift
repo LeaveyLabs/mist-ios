474368d4ccf28e8146a86c2a50408385e114c9a0
kevinsun127 <kevinsun@usc.edu>
2022-06-19 16:02:11 -0700

    delete vote

--- Supporting Files/APIs/VoteAPI.swift
+++ Supporting Files/APIs/VoteAPI.swift
@@ -25,6 +25,15 @@ class VoteAPI {
         return try JSONDecoder().decode([Vote].self, from: data)
     }
     
+    // Gets the ID of the vote with a given username and post_id
+    static func fetchVotesByVoterAndPost(voter:Int, post:Int) async throws -> [Vote] {
+        // Fetch the vote from the API endpoint
+        let url = "\(BASE_URL)\(PATH_TO_VOTE_MODEL)?\(VOTER_PARAM)=\(voter)&\(POST_PARAM)=\(post)"
+        let (data, _) = try await BasicAPI.baiscHTTPCallWithToken(url: url, jsonData: Data(), method: HTTPMethods.GET.rawValue)
+        // Deserialize to get original vote
+        return try JSONDecoder().decode([Vote].self, from: data)
+    }
+    
     // Post vote to database
     static func postVote(voter:Int, post:Int) async throws -> Vote {
         let url = "\(BASE_URL)\(PATH_TO_VOTE_MODEL)"
@@ -38,21 +47,8 @@ class VoteAPI {
     }
 
     // Deletes vote from database
-    static func deleteVote(voter:Int, post:Int) async throws {
-        // Get the ID of the vote, then delete it from the database
-        let votes = try await getVoteByVoterAndPost(voter: voter, post: post)
-        let id = votes[0].id
+    static func deleteVote(id:Int) async throws {
         let url = "\(BASE_URL)\(PATH_TO_VOTE_MODEL)\(id)/"
         let (_, _) = try await BasicAPI.baiscHTTPCallWithToken(url: url, jsonData: Data(), method: HTTPMethods.DELETE.rawValue)
     }
-    
-    // Gets the ID of the vote with a given username and post_id
-    static private func getVoteByVoterAndPost(voter:Int, post:Int) async throws -> [Vote] {
-        // Fetch the vote from the API endpoint
-        let url = "\(BASE_URL)\(PATH_TO_VOTE_MODEL)?\(VOTER_PARAM)=\(voter)&\(POST_PARAM)=\(post)"
-        let (data, _) = try await BasicAPI.baiscHTTPCallWithToken(url: url, jsonData: Data(), method: HTTPMethods.GET.rawValue)
-        // Deserialize to get original vote
-        return try JSONDecoder().decode([Vote].self, from: data)
-    }
-
 }
