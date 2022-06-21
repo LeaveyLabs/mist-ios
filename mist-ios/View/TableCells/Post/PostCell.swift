7bc83b490e82c04f2db17c8e1860dd894d16085e
adamvnovak <adamnova@usc.edu>
2022-06-21 07:40:29 -0700

    forcing swift messages to run on the main thread

--- mist-ios/View/TableCells/Post/PostCell.swift
+++ mist-ios/View/TableCells/Post/PostCell.swift
@@ -35,6 +35,7 @@ class PostCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .default, reuseIdentifier: Constants.SBID.Cell.Post)
         
+        print("INITING POST CELL")
         postView = PostView()
         postView.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(postView)
@@ -42,13 +43,11 @@ class PostCell: UITableViewCell {
         let verticalConstraints = [
             postView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
             postView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),]
-        verticalConstraints.forEach { constraint in constraint.priority = .defaultHigh } //in order to prevent conflicts with "UIView-Encapsulated-Layout-Height" of cell's contentView upon initial load
+        verticalConstraints.forEach { $0.priority = .defaultHigh } //in order to prevent conflicts with "UIView-Encapsulated-Layout-Height" of cell's contentView upon initial load
         NSLayoutConstraint.activate([
             postView.widthAnchor.constraint(equalTo: contentView.widthAnchor, constant: -52),
             postView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor, constant: 2),
-            verticalConstraints[0],
-            verticalConstraints[1],
-        ])
+        ] + verticalConstraints)
     }
         
     required init?(coder aDecoder: NSCoder) {
