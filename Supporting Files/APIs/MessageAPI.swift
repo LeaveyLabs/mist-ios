62b6785627453da4b374eb3dbca0b9ab4ce3a1a2
kevinsun127 <kevinsun@usc.edu>
2022-06-21 21:36:21 -0700

    fetch by sender and receiver

--- Supporting Files/APIs/MessageAPI.swift
+++ Supporting Files/APIs/MessageAPI.swift
@@ -12,6 +12,18 @@ class MessageAPI {
     static let SENDER_PARAM = "sender"
     static let RECEIVER_PARAM = "receiver"
     
+    static func fetchMessagesBySender(sender:Int) async throws -> [Message] {
+        let url = "\(BASE_URL)\(PATH_TO_BLOCK_MODEL)?\(SENDER_PARAM)=\(sender)"
+        let (data, _) = try await BasicAPI.baiscHTTPCallWithToken(url: url, jsonData: Data(), method: HTTPMethods.GET.rawValue)
+        return try JSONDecoder().decode([Message].self, from: data)
+    }
+    
+    static func fetchMessagesByReceiver(receiver:Int) async throws -> [Message] {
+        let url = "\(BASE_URL)\(PATH_TO_BLOCK_MODEL)?\(RECEIVER_PARAM)=\(receiver)"
+        let (data, _) = try await BasicAPI.baiscHTTPCallWithToken(url: url, jsonData: Data(), method: HTTPMethods.GET.rawValue)
+        return try JSONDecoder().decode([Message].self, from: data)
+    }
+    
     static func fetchMessagesBySenderAndReceiver(sender:Int, receiver:Int) async throws -> [Message] {
         let url = "\(BASE_URL)\(PATH_TO_BLOCK_MODEL)?\(SENDER_PARAM)=\(sender)&\(RECEIVER_PARAM)=\(receiver)"
         let (data, _) = try await BasicAPI.baiscHTTPCallWithToken(url: url, jsonData: Data(), method: HTTPMethods.GET.rawValue)
