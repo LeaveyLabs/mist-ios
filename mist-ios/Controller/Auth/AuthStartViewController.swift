ede2960bfedb4b7bc1970af8fe08292932916d34
kevinsun127 <kevinsun@usc.edu>
2022-07-17 18:33:18 -0700

    new agreement text view

--- mist-ios/Controller/Auth/AuthStartViewController.swift
+++ mist-ios/Controller/Auth/AuthStartViewController.swift
@@ -6,16 +6,19 @@
 //
 
 import UIKit
+import Foundation
 
-class AuthStartViewController: UIViewController {
+class AuthStartViewController: UIViewController, UITextViewDelegate {
     
     var mistWideLogoView: MistWideLogoView!
+    @IBOutlet weak var agreementTextView: UITextView!
     @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
     
     override func loadView() {
         super.loadView()
         loadMistLogo()
+        setupAgreementTextView()
     }
     
     func loadMistLogo() {
@@ -29,6 +32,38 @@ class AuthStartViewController: UIViewController {
             mistWideLogoView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
             mistWideLogoView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
         ])
+    }
+    
+    func setupAgreementTextView() {
+        if let agreementText = agreementTextView.text {
+            let attributedText = NSMutableAttributedString(string: agreementText)
+            
+            let termsOfUseUrl = URL(string: "https://www.getmist.app/terms-of-use")!
+            if let termsOfUseRange = agreementText.range(of: "Terms of Use") {
+                attributedText.setAttributes([.link: termsOfUseUrl], range: NSRange(termsOfUseRange, in: agreementText))
+            }
+            
+            let privacyPolicyUrl = URL(string: "https://www.getmist.app/privacy-policy")!
+            if let privacyPolicyRange = agreementText.range(of: "Privacy Policy") {
+                attributedText.setAttributes([.link: privacyPolicyUrl], range: NSRange(privacyPolicyRange, in: agreementText))
+            }
+            
+            agreementTextView.attributedText = attributedText
+            agreementTextView.font = UIFont(name: "Avenir", size: 12)
+            agreementTextView.textColor = UIColor.lightGray
+            agreementTextView.isEditable = false
+            agreementTextView.delegate = self
+            agreementTextView.isUserInteractionEnabled = true
+            agreementTextView.textAlignment = NSTextAlignment.center
+            agreementTextView.linkTextAttributes = [
+                .foregroundColor: UIColor.blue,
+                .underlineStyle: NSUnderlineStyle.single.rawValue
+            ]
+        }
+    }
+    
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        agreementTextView.selectedTextRange = nil
     }
     
     override func viewDidLoad() {
