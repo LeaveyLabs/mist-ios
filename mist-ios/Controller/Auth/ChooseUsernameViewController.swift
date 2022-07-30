16b4927d25035735498e41c4e99698dc00e9568e
adamvnovak <adamnova@usc.edu>
2022-07-30 14:43:42 +0900

    reduced sizing of custom swift messages card alert because text was being cut off

--- mist-ios/Controller/Auth/ChooseUsernameViewController.swift
+++ mist-ios/Controller/Auth/ChooseUsernameViewController.swift
@@ -116,7 +116,7 @@ class ChooseUsernameViewController: KUIViewController, UITextFieldDelegate {
     func handleFailure(_ error: Error) {
         isSubmitting = false
         isValidInput = false
-        CustomSwiftMessages.displayError("That username is already in use", "Please choose another one")
+        CustomSwiftMessages.displayError("Username already in use", "Please choose another one")
     }
     
     func validateInput() {
